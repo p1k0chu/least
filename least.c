3f5bc2e1a50ea22bce14a08ceca8116a084f9c17
p1k0chu <p1k0chu.vim@gmail.com>
2026-01-29 10:38:15 +0200

    Initial commit

---
+++ least.c
@@ -0,0 +1,24 @@
+/*
+ * least.c - pager that does the least a pager
+ * needs to do. it does less than less
+ * Copyright (C) 2026 p1k0chu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    return EXIT_SUCCESS;
+}
